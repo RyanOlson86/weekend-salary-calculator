57417ef6c5b205a83ef3722e5425b2b863f57689
Ryan Olson <ryan.olson86@yahoo.com>
2023-12-01 16:56:41 -0600

    created function for submit button. need so work on the outputs

--- salary.js
+++ salary.js
@@ -1,2 +1,34 @@
-console.log('jafnds')
+console.log("jafnds");
 
+// Function to take in info after submit button and push to table
+// Look into childNodes
+let formInputs = document.getElementById('formInputs');
+console.log("formInputs Obj:", formInputs);
+
+function submitInfo(event) {
+  console.log("submit button works");
+
+  // Target tableBody
+  let tableBody = document.getElementById("tableBody");
+
+  // !this works
+
+  let firstName = document.getElementById('firstNameInput').value;
+  let lastName = document.getElementById('lastNameInput').value;
+  console.log('first name from form', firstName);
+  console.log('last name from form', lastName);
+
+  // push 'string' to html. Should be able to concatenate using different inputs. Maybe use a loop?
+//   tableBody.innerHTML += `
+//     <tr>
+//       <td>First Name</td>
+//       <td>Last Name</td>
+//       <td>ID</td>
+//       <td>Title</td>
+//       <td>Annual Salary</td>
+//       <td></td>
+//     </tr>`;
+
+  // Use this to stop submit button from refreshing page
+  event.preventDefault();
+}
