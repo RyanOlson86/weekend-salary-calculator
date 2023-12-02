4e02715c062243aa52e2d4a26f0b8467641efffe
Ryan Olson <ryan.olson86@yahoo.com>
2023-12-02 14:13:34 -0600

    added code to add values to table

--- salary.js
+++ salary.js
@@ -7,14 +7,33 @@ console.log("formInputs Obj:", formInputs);
 
 function submitInfo(event) {
   console.log("submit button works");
-
   // Target tableBody
   let tableBody = document.getElementById("tableBody");
 
-  // !this works
-
   let firstName = document.getElementById('firstNameInput').value;
   let lastName = document.getElementById('lastNameInput').value;
+  let idInput = document.getElementById('idInput').value
+  let titleInput = document.getElementById('titleInput').value
+  let annualSalaryInput = document.getElementById('annualSalaryInput').value
+  
+  // code to add new table elements
+  let row = tableBody.insertRow(0);
+  let cell0 = row.insertCell(0);
+  let cell1 = row.insertCell(1);
+  let cell2 = row.insertCell(2);
+  let cell3 = row.insertCell(3);
+  let cell4 = row.insertCell(4);
+  // let cell5 = row.insertCell(5);
+
+  cell0.innerHTML =firstName;
+  cell1.innerHTML =lastName;
+  cell2.innerHTML =idInput;
+  cell3.innerHTML =titleInput;
+  cell4.innerHTML =annualSalaryInput;
+  // cell5.innerHTML =firstName;
+
+
+
   console.log('first name from form', firstName);
   console.log('last name from form', lastName);
 
