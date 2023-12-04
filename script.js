af21ec5d99adde1a759e582fbf7d9b5345e55ce5
Ryan Olson <ryan.olson86@yahoo.com>
2023-12-03 19:29:48 -0600

    added formatting for US dollars

--- script.js
+++ script.js
@@ -16,14 +16,24 @@ function submitInfo(event) {
   let idInput = document.getElementById("idInput").value;
   let titleInput = document.getElementById("titleInput").value;
   let annualSalaryInput = document.getElementById("annualSalaryInput").value;
+  
 
   monthlySalary += annualSalaryInput / 12;
-  let monthlySalaryPath = document.getElementById('over-budget');
-  if(monthlySalary>20000){
-    document.querySelector('footer').classList.add('over-budget');
+  let monthlySalaryPath = document.getElementById("over-budget");
+  if (monthlySalary > 20000) {
+    document.querySelector("footer").classList.add("over-budget");
   }
+
+  //Format annualSalaryInput into currency
+  let USDollar = Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+  annualSalaryInput = USDollar.format(annualSalaryInput);
+  let monthlySalaryFormat = USDollar.format(monthlySalary);
+
   // Add monthlySalary to footer
-  monthlySalaryPath.innerHTML = `<p>Total Monthly: ${monthlySalary}</p>`
+  monthlySalaryPath.innerHTML = `<p>Total Monthly: ${monthlySalaryFormat}</p>`;
 
   // code to add new table elements
   let row = tableBody.insertRow(0);
@@ -39,13 +49,13 @@ function submitInfo(event) {
   cell2.innerHTML = idInput;
   cell3.innerHTML = titleInput;
   cell4.innerHTML = annualSalaryInput;
-  cell5.innerHTML ='<button onclick="deleteInfo(event)">❌</button>';
+  cell5.innerHTML = '<button onclick="deleteInfo(event)">❌</button>';
 
   // clear the form
-  document.getElementById("formInputs").reset()
+  document.getElementById("formInputs").reset();
   // Use this to stop submit button from refreshing page
   event.preventDefault();
 }
-function deleteInfo(event){
+function deleteInfo(event) {
   event.target.parentNode.parentNode.remove();
 }
